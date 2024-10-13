07c6e4048d5c4283b45594f14b670f35fc8854de
Ayush <ayushrawat00112@gmail.com>
2024-10-13 16:13:11 +0530

    Full project push, optimization remain..

---
+++ src/firebase.js
@@ -0,0 +1,42 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from 'firebase/app';
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: 'mern-estate.firebaseapp.com',
+//   projectId: 'mern-estate',
+//   storageBucket: 'mern-estate.appspot.com',
+//   messagingSenderId: '1078482850952',
+//   appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
+
+
+// --------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "homewise-5d665.firebaseapp.com",
+  projectId: "homewise-5d665",
+  storageBucket: "homewise-5d665.appspot.com",
+  messagingSenderId: "658841494403",
+  appId: "1:658841494403:web:52c5bcad4181c36dfd4cb6"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-10 text-slate-800'>HomeWise</h1>
+      <p className='mb-4 text-xl text-slate-700'>HomeWise is a top real estate agency dedicated to assisting clients in buying, selling, and renting properties in highly sought-after neighborhoods. Our team is committed to delivering outstanding service and ensuring a seamless experience throughout the buying and selling process.</p>
+      <p className='mb-4 text-xl text-slate-700'>
+      Our objective is to guide our clients in reaching their real estate goals by offering expert advice, personalized service, and in-depth knowledge of the local market. Whether you're looking to buy, sell, or rent a property, we're here to support you every step of the way.
+      </p>
+      <p className='mb-4 text-xl text-slate-700'>Our team brings extensive experience and expertise in the real estate industry, and we are dedicated to delivering the highest level of service to our clients. We believe that buying or selling a property should be a positive and rewarding journey, and we are committed to making that a reality for every one of our clients.</p>
+    </div>
+  )
+}
